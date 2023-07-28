6f65d54389149c606409ef675992f7bea7f93e73
Matt Jarjoura <jarjoura@gmail.com>
2023-07-28 14:15:44 -0700

    Initial Commit

---
+++ NavigationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NavigationDemo
+//
+//  Created by Matt Jarjoura on 7/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NavigationDemo/NavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationDemoApp.swift
+//  NavigationDemo
+//
+//  Created by Matt Jarjoura on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
