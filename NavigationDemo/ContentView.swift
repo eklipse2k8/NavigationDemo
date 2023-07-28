083a10de9974767b5f7bdb6171dbc5e6b106fd67
Matt Jarjoura <jarjoura@gmail.com>
2023-07-28 14:30:07 -0700

    Navigation

--- NavigationDemo/ContentView.swift
+++ NavigationDemo/ContentView.swift
@@ -7,18 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ChildView: View {
+  var body: some View {
+    ScrollView {
+      Text("I am the child")
     }
+    .navigationTitle("Child")
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarBackButtonHidden(false)
+  }
+}
+
+struct ContentView: View {
+  @State var path: NavigationPath = .init()
+
+  var body: some View {
+    NavigationStack {
+      List {
+        NavigationLink {
+          // Link here
+          ChildView()
+        } label: {
+          Label(
+            title: { Text("Link 1") },
+            icon: { Image(systemName: "1.circle") }
+          )
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+
+        NavigationLink {
+          // Link Here
+        } label: {
+          Label(
+            title: { Text("Link 2") },
+            icon: { Image(systemName: "2.circle") }
+          )
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+      }
+      .navigationTitle("Home Screen")
+      .navigationBarTitleDisplayMode(.inline)
+    }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
